481040883db97a1b099f39fb612f7b4d3d747d4b
Dulanjaya Bhanu <146608946+dulanjayabhanu@users.noreply.github.com>
2024-06-03 10:30:23 +0530

    Update connection.php in master branch

--- connection.php
+++ connection.php
@@ -5,7 +5,7 @@
         
         public static function setUpConnection(){
             if(!isset(Database::$connection)){
-                Database::$connection = new mysqli("localhost","root","459@SofT#374","helphub","3306");
+                Database::$connection = new mysqli("localhost","root","","helphub","3306");
             }
         }
 
